eca13200f990bfd62c617a8bf54b57399348e6b7
Fred Kleuver <fred@avurad.nl>
2026-08-09 03:26:20 +0200

    Attribute worker transport events by language client

--- packages/vscode/src/extension.ts
+++ packages/vscode/src/extension.ts
@@ -45,6 +45,7 @@ const createLanguageClient = (logger: ClientLogger): LanguageClientFactory => (
   serverModule: string,
   clientOptions: LanguageClientOptions,
 ): LanguageClient => {
+  const transportLogger = logger.child("client", { id, name });
   if (shouldUseExperimentalWorkerTransport()) {
     return new LanguageClient(
       id,
@@ -53,26 +54,26 @@ const createLanguageClient = (logger: ClientLogger): LanguageClientFactory => (
         onEvent: (event) => {
           switch (event.type) {
             case "online":
-              logger.debug("Experimental Worker transport is online");
+              transportLogger.debug("Experimental Worker transport is online");
               break;
             case "stdout":
-              logger.debug("Experimental Worker stdout", { text: event.text.trimEnd() });
+              transportLogger.debug("Experimental Worker stdout", { text: event.text.trimEnd() });
               break;
             case "stderr":
-              logger.warn("Experimental Worker stderr", { text: event.text.trimEnd() });
+              transportLogger.warn("Experimental Worker stderr", { text: event.text.trimEnd() });
               break;
             case "error":
-              logger.error("Experimental Worker transport failed", undefined, event.error);
+              transportLogger.error("Experimental Worker transport failed", undefined, event.error);
               break;
             case "exit":
               if (event.code === 0) {
-                logger.debug("Experimental Worker transport exited", { code: event.code });
+                transportLogger.debug("Experimental Worker transport exited", { code: event.code });
               } else {
-                logger.warn("Experimental Worker transport exited abnormally", { code: event.code });
+                transportLogger.warn("Experimental Worker transport exited abnormally", { code: event.code });
               }
               break;
             case "force-terminate":
-              logger.warn("Experimental Worker transport exceeded its shutdown grace", {
+              transportLogger.warn("Experimental Worker transport exceeded its shutdown grace", {
                 graceMilliseconds: event.graceMilliseconds,
               });
               break;
